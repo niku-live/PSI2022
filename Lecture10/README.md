d54d04f420961e97db0e1827f728a3ae5b90263c
Karolis Uosis <kuosis@alna.lt>
2022-12-06 15:57:15 +0200

    Lecture 10 content

---
+++ Lecture10/README.md
@@ -0,0 +1,9 @@
+# Lecture 10
+
+## What was discussed
+
+- [PLINQ examples](PLinq/ParallelLinq.dib)
+- [Testing](Testing/Tests.dib)
+  - [xUnit](Testing/xUnitExampleSolution/)
+  - [nUnit](Testing/nUnitExampleSolution/)
+  - [MSTest](Testing/MSTestExampleSolution/)
---
+++ Lecture10/Testing/MSTestExampleSolution/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            // Arrange
+            
+            // Act
+            var result = _primeService.IsPrime(1);
+
+            // Assert
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+
+        #region Sample_TestCode
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            // Arrange
+            
+            // Act
+            var result = _primeService.IsPrime(value);
+
+            // Assert
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+        #endregion
+    }
+}
